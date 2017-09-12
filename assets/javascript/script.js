13ea2ba5d2a8a5cd84ac9223cbaa04f981297647
Michael Wolfe <michael.wolfe101@gmail.com>
2017-09-12 11:09:38 -0400

    added detailed comments, altered some HTML and CSS, included media queries, minor text fixes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,30 @@
-var word;
-
+var word;		//represents word to be guessed
 var guess;		//user guess
+var counter;		//counts correct letters
+
 var letters = [];		//correctly guessed letters
 var wrongLetters = [];		//incorrectly guessed letters
-var counter;		//counts correct letters
-var losses = 0;
-var wins = 0;
+var losses = 0;		//totall losses
+var wins = 0;		//total wins
+
+//Audio
 var audio = new Audio("assets/audio/music.mp3");
 var blaster = new Audio("assets/audio/blaster.mp3");
 var vader = new Audio("assets/audio/father.mp3");
 var r2d2 = new Audio("assets/audio/r2d2.mp3");
 
-
-
+//displays wins and losses on page
 document.getElementById("losses").innerHTML = losses;
 document.getElementById("wins").innerHTML = wins;
 
+//list of random words
 var wordListA = ["alderaan", "asteroid", "coruscant", "dagobah", "hoth", "kashyyyk", "naboo", "tatooine", "droid", "atat", "blaster", 
 "podracer", "sandcrawler", "clone", "luke", "leia", "vader", "biggs", "dooku", "dak", "emperor", "palpatine", "fin", "rey", "tarkin", 
 "han", "chewbacca", "jabba", "greedo", "lando", "yoda", "padme", "maul", "wedge", "yoda", "bantha", "sarlacc", "wampa", "tauntaun", 
 "wookie", "empire", "rebel", "light", "dark", "bowcaster", "phasma", "lightsaber"];
 
-var wordList = String.prototype.toUpperCase.apply(wordListA).split(","); //TRYING TO MAKE LIST UPPERCASE
+//changes words to all uppercase
+var wordList = String.prototype.toUpperCase.apply(wordListA).split(",");
 
 //randomly chooses a word from the array and replaces letters with underscores
 function start() {
@@ -32,20 +35,19 @@ function start() {
 		letters[i] = "__";
 	}
 
+	//blank word is displayed on page
 	document.getElementById("answer").innerHTML = letters.join(" ");
-	console.log(word);
-
-
+	console.log(word);		//for cheating
 }
 
 
-//checks if letter is in the word or not
+//main game function
 function Game() {
 	audio.play();
 
 	document.onkeyup = function(event) {
 		if (!(event.which <= 90 && event.which >= 65)) return
-		guess = event.key.toUpperCase();
+			guess = event.key.toUpperCase();
 		var found = false;
 		var hangmanPicture = document.getElementById("HangmanImg");
 		console.log(found);
@@ -56,6 +58,7 @@ function Game() {
 				found = true;
 			} 
 		}
+
 		//wrong letters go into the wrongLetters array and are displayed
 		if (found && letters.join("") !== word) return;
 		if (wrongLetters.indexOf(guess) < 0) {
@@ -67,25 +70,29 @@ function Game() {
 			console.log(counter);
 			document.getElementById("counter").innerHTML = counter;
 			//when counter reaches 0 it's Game Over
-			//+1 to the losses if 6 words are missed
-			
-			if (counter > 0 && letters.join("") === word) {				//THE ISSUE
+
+			//+1 to wins if word is guessed
+			if (counter > 0 && letters.join("") === word) {
 				document.getElementById("wins").innerHTML = wins + 1;
 				console.log(wins);
 				r2d2.play();
-				confirm("YOU WIN! The word was " + word.toUpperCase() + " Play again?");
-				wins++;
-				counter = 6;
-				letters = [ ];
-				wrongLetters = [ ];
-				start();
+				confirm("YOU WIN! The word was " + word.toUpperCase() + " Play again?"); {
+					//new round
+					wins++;
+					counter = 6;
+					letters = [ ];
+					wrongLetters = [ ];
+					start();
+				}
 			}
 
+			//+1 to losses if counter reaches 0
 			else if (counter === 0) {
 				document.getElementById("losses").innerHTML = losses + 1;
 				console.log(losses);
 				vader.play();
 				confirm("YOU LOSE... The word was " + word.toUpperCase() + " play again?"); {
+					//new round
 					losses++;
 					counter = 6;
 					letters = [];
@@ -94,7 +101,7 @@ function Game() {
 				}
 			}
 
-
+			//adds new hangman image as for every incorrect guess/counter ticks down
 			if (counter == 6) {
 				hangmanPicture.src = "assets/images/1.png";
 			}
@@ -122,6 +129,7 @@ function Game() {
 	}
 }
 
+//press any key to start function
 function pressKey() {
 	document.onkeyup = function(event) {
 		document.getElementById("press-to-start").innerHTML = " ";
@@ -131,5 +139,5 @@ function pressKey() {
 	}
 }
 
-
+//calls pressKey function
 pressKey();
